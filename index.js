4ad8f62d77da0b86fe0148192a6f627b59cf42d0
Samar <samarbadriddinov.tv@gmail.com>
2022-12-23 16:36:53 +0200

    Handlebars

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 import express from 'express'
 import path, {dirname} from 'path'
 import {fileURLToPath} from 'url'
+import {engine} from 'express-handlebars'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
 const app = express()
 
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars')
+app.set('views', './views')
+
 app.get('/', (req, res) => {
-	res.status(200)
-	res.sendFile(path.join(__dirname, 'views', 'index.html'))
+	res.render('index')
 })
 
 app.get('/about', (req, res) => {
-	res.status(200)
-	res.sendFile(path.join(__dirname, 'views', 'about.html'))
+	res.render('about')
 })
 
 const PORT = process.env.PORT || 4100
