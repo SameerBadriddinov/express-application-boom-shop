842a43ba7aac71d308143abe71474d54c88c72b0
Samar <samarbadriddinov.tv@gmail.com>
2022-12-24 13:55:52 +0200

    MongoDb ulash, .env

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import express from 'express'
 import {create} from 'express-handlebars'
+import mongoose from 'mongoose'
+import * as dotenv from 'dotenv'
+
+// ROUTES
 import AuthRoutes from './routes/auth.js'
 import ProductsRoutes from './routes/products.js'
+
+dotenv.config()
 
 const app = express()
 
@@ -13,9 +19,21 @@ app.set('views', './views')
 
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
+app.use(express.json())
 
 app.use(AuthRoutes)
 app.use(ProductsRoutes)
 
-const PORT = process.env.PORT || 4100
-app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`))
+const startApp = () => {
+	try {
+		mongoose.set('strictQuery', false)
+		mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true}, () => console.log('Mongo DB connected'))
+
+		const PORT = process.env.PORT || 4100
+		app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`))
+	} catch (error) {
+		console.log(error)
+	}
+}
+
+startApp()
