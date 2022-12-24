a2a56985f064e4ca0f6c89b97cc61daaf777c050
Samar <samarbadriddinov.tv@gmail.com>
2022-12-24 13:27:42 +0200

    Static folder

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const hbs = create({defaultLayout: 'main', extname: 'hbs'})
 app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', './views')
+
+app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
 
 app.use(AuthRoutes)
