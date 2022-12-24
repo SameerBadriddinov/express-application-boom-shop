282a3d5c824d7d17fb2f9ded69133b398ec6a7c1
Samar <samarbadriddinov.tv@gmail.com>
2022-12-24 11:28:26 +0200

    Dynamic title

--- routes/auth.js
+++ routes/auth.js
@@ -2,11 +2,17 @@ import {Router} from 'express'
 const router = Router()
 
 router.get('/login', (req, res) => {
-	res.render('login')
+	res.render('login', {
+		title: 'Login | Sammi',
+		isLogin: true,
+	})
 })
 
 router.get('/register', (req, res) => {
-	res.render('register')
+	res.render('register', {
+		title: 'Register | Sammi',
+		isRegister: true,
+	})
 })
 
 export default router
--- routes/products.js
+++ routes/products.js
@@ -2,15 +2,23 @@ import {Router} from 'express'
 const router = Router()
 
 router.get('/', (req, res) => {
-	res.render('index')
+	res.render('index', {
+		title: 'Boom shop | Sammi',
+	})
 })
 
 router.get('/products', (req, res) => {
-	res.render('products')
+	res.render('products', {
+		title: 'Products | Sammi',
+		isProducts: true,
+	})
 })
 
 router.get('/add', (req, res) => {
-	res.render('add')
+	res.render('add', {
+		title: 'Add products',
+		isAdd: true,
+	})
 })
 
 export default router
