8969433e5749f7d3ce52071e7ff19adc03adab76
Samar <samarbadriddinov.tv@gmail.com>
2022-12-28 14:04:05 +0200

    Bosh sahifa

--- routes/products.js
+++ routes/products.js
@@ -5,9 +5,12 @@ import Product from '../models/Product.js'
 
 const router = Router()
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+	const products = await Product.find().lean()
+
 	res.render('index', {
 		title: 'Boom shop | Sammi',
+		products: products,
 	})
 })
 
