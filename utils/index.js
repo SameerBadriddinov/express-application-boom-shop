2bcbaa2d33c5d92e0e5b32dcfc69c5df6b05a7bb
Samar <samarbadriddinov.tv@gmail.com>
2022-12-29 12:47:23 +0200

    Qo'shimcha funktsiya

--- utils/index.js
+++ utils/index.js
@@ -1,3 +1,5 @@
+import moment from 'moment'
+
 export default {
 	ifequal(a, b, options) {
 		if (a == b) {
@@ -8,5 +10,8 @@ export default {
 	},
 	getFullNameFirstCharacter(firstName, lastName) {
 		return firstName.charAt(0) + lastName.charAt(0)
+	},
+	formatDate(date) {
+		return moment(date).format('DD MMM, YYYY')
 	},
 }
