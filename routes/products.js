d4d62f723cc434e9fa1f31496ed0096a1df2f184
Samar <samarbadriddinov.tv@gmail.com>
2022-12-29 14:15:42 +0200

    Delete product

--- routes/products.js
+++ routes/products.js
@@ -78,4 +78,11 @@ router.post('/edit-product/:id', async (req, res) => {
 	res.redirect('/products')
 })
 
+router.post('/delete-product/:id', async (req, res) => {
+	const id = req.params.id
+
+	await Product.findByIdAndRemove(id)
+	res.redirect('/')
+})
+
 export default router
