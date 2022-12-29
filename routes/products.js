cd2cf13c57ac305418848634575a66632fc4cd29
Samar <samarbadriddinov.tv@gmail.com>
2022-12-29 13:57:25 +0200

    Edit product

--- routes/products.js
+++ routes/products.js
@@ -43,6 +43,16 @@ router.get('/product/:id', async (req, res) => {
 	})
 })
 
+router.get('/edit-product/:id', async (req, res) => {
+	const id = req.params.id
+	const product = await Product.findById(id).populate('user').lean()
+
+	res.render('edit-product', {
+		product: product,
+		errorEditProduct: req.flash('errorEditProduct'),
+	})
+})
+
 router.post('/add-products', userMiddleware, async (req, res) => {
 	const {title, description, image, price} = req.body
 	if (!title || !description || !image || !price) {
@@ -53,6 +63,19 @@ router.post('/add-products', userMiddleware, async (req, res) => {
 
 	await Product.create({...req.body, user: req.userId})
 	res.redirect('/')
+})
+
+router.post('/edit-product/:id', async (req, res) => {
+	const {title, description, image, price} = req.body
+	const id = req.params.id
+	if (!title || !description || !image || !price) {
+		req.flash('errorEditProduct', 'All fields is required')
+		res.redirect(`/edit-product/${id}`)
+		return
+	}
+
+	await Product.findByIdAndUpdate(id, req.body, {new: true})
+	res.redirect('/products')
 })
 
 export default router
